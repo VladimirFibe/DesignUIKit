42509f682e9989ba021509d1d1b22cc1f5672c37
Vladimir <motiw@icloud.com>
2022-08-15 15:37:52 +0600

    Initial Commit

---
+++ DesignUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesignUIKit
+//
+//  Created by Vladimir Fibe on 15.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
